60500309abdfdf6c7886c95f2f3246e6a6a32266
danielcopelin <danielcopelin@gmail.com>
2020-11-23 07:59:18 +1000

    working on debugging simulation

--- wb_nodes.py
+++ wb_nodes.py
@@ -4,6 +4,11 @@ import pandas as pd
 from wb_simulation import Simulation
 
 
+class Base:
+    def __str__(cls):
+        return f"{type(cls)}: {cls.name}"
+
+
 class Factor(float):
     """Float type with value constrained between 0.0 and 1.0.
     """
@@ -20,64 +25,124 @@ class Factor(float):
         return float.__new__(cls, *value)
 
 
-class Storage:
+class Storage(Base):
     def __init__(
         self,
         name: str,
         simulation: Simulation,
-        area: float,
-        max_volume: float,
-        initial_volume: float,
-        demands: list,
-        leakage: float,
-        outlet: "Storage",
+        area: float,  # m2
+        max_volume: float,  # m3
+        initial_volume: float,  # m3
+        leakage_rate: float,  # m3/timestep
+        outlet: Union["Storage", None],
     ):
         self.name = name
         self.simulation = simulation
         self.area = area
         self.max_volume = max_volume
         self.initial_volume = initial_volume
-        self.demands = demands
-        self.leakage = leakage
+        self.leakage_rate = leakage_rate
+        self.outlet = outlet
 
         # initialise series to contain results
         self.inflow = pd.Series(
-            index=simulation.rainfall.index, name=f"{self.name}_storage_inflow"
+            index=simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_storage_inflow",
         )
         self.volume = pd.Series(
-            index=simulation.rainfall.index, name=f"{self.name}_storage_volume"
+            index=simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_storage_volume",
         )
         self.supplied = pd.Series(
-            index=simulation.rainfall.index, name=f"{self.name}_storage_supplied"
+            index=simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_storage_supplied",
         )
         self.overflow = pd.Series(
-            index=simulation.rainfall.index, name=f"{self.name}_storage_overflow"
+            index=simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_storage_overflow",
         )
+        self.et = pd.Series(
+            index=simulation.rainfall.index, data=0.0, name=f"{self.name}_storage_et"
+        )
+        self.leakage = pd.Series(
+            index=simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_storage_leakage",
+        )
+
+        # update initial volume
+        self.volume[0] = self.initial_volume
 
         # register with simulation
         self.simulation.storages[self.name] = self
 
-    def step(self, time):
-        pass
+    def step(self):
+        time = self.simulation.time
+        time_previous = self.simulation.time_previous
 
-    # this will have to be integrated with the stepping to properly handle multiple demands from one storage
+        # bring forward previous volume
+        self.volume.loc[time] = self.volume.loc[time_previous]
+
+        # handle rainfall on storage & record in results
+        if self.area > 0:
+            rainfall_volume = self.simulation.rainfall.loc[time] * self.area / 1000.0
+            self.volume.loc[time] = self.volume.loc[time] + rainfall_volume
+            self.inflow.loc[time] = self.inflow.loc[time] + rainfall_volume
+
+        # handle et & record in results
+        if self.area > 0:
+            et_volume = max(
+                self.volume.loc[time],
+                self.simulation.pet.loc[time] * self.area / 1000.0,
+            )
+            self.volume.loc[time] = self.volume.loc[time] - et_volume
+            self.et.loc[time] = self.et.loc[time] + et_volume
+
+        # handle leakage & record in results
+        if self.leakage_rate > 0:
+            leakage = max(self.volume.loc[time], self.leakage_rate)
+            self.volume.loc[time] = self.volume.loc[time] - leakage
+            self.leakage.loc[time] = leakage
+
+        # calculate and route any overflow
+        if self.volume.loc[time] > self.max_volume:
+            overflow = self.volume.loc[time] - self.max_volume
+            self.volume.loc[time] = self.max_volume
+            self.overflow.loc[time] = overflow
+        else:
+            overflow = 0.0
+
+        if self.outlet:
+            self.outlet.put(overflow)
+
     def take(self, requested_volume):
-        current_volume = self.volume[-1]
+        time = self.simulation.time
+        current_volume = self.volume[time]
 
         if requested_volume <= current_volume:
             supplied_volume = requested_volume
         else:
             supplied_volume = current_volume
 
-        self.volume.append(current_volume - supplied_volume)
+        # update storage volume and record supplied flows in results
+        self.volume.loc[time] = self.volume.loc[time] - supplied_volume
+        self.supplied.loc[time] = self.supplied.loc[time] + supplied_volume
 
         return supplied_volume
 
     def put(self, incoming_volume):
-        pass
+        time = self.simulation.time
+
+        # update storage volume and record incoming flows in results
+        self.volume.loc[time] = self.volume.loc[time] + incoming_volume
+        self.inflow.loc[time] = self.inflow.loc[time] + incoming_volume
 
 
-class Surface:
+class SurfaceAWBM(Base):
     def __init__(
         self,
         name: str,
@@ -85,65 +150,169 @@ class Surface:
         area: float,
         imp_frac: float,
         outlet: Storage,
-        a1: Factor = 0.134,
-        a2: Factor = 0.433,
+        a1: float = 0.134,
+        a2: float = 0.433,
         c1: float = 7.0,
         c2: float = 70.0,
         c3: float = 150.0,
-        bfi: Factor = 0.35,
-        kbase: Factor = 0.95,
-        ksurf: Factor = 0.35,
+        bfi: float = 0.35,
+        kbase: float = 0.95,
+        ksurf: float = 0.35,
+        i: float = 1.0,
     ):
 
+        self.name = name
         self.simulation = simulation
-
         self.area = area
-        self.imp_frac = imp_frac
+        self.imp_frac = Factor(imp_frac)
+        self.outlet = outlet
 
-        self.a1 = a1
-        self.a2 = a2 if (a1 + a2 <= 1) else 1 - a1
+        self.a1 = Factor(a1)
+        self.a2 = Factor(a2) if (a1 + a2 <= 1) else 1 - a1
         self.a3 = 1.0 - a1 - a2
         self.c1 = c1
         self.c2 = c2
         self.c3 = c3
-        self.bfi = bfi
-        self.kbase = kbase
-        self.ksurf = ksurf
-
-        self.outlet = outlet
+        self.bfi = Factor(bfi)
+        self.kbase = Factor(kbase)
+        self.ksurf = Factor(ksurf)
+        self.i = i
 
         # initialise series to contain results
         self.store1 = pd.Series(
-            index=simulation.rainfall.index, name=f"{self.name}_surface_store1"
+            index=self.simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_surface_store1",
         )
         self.store2 = pd.Series(
-            index=simulation.rainfall.index, name=f"{self.name}_surface_store2"
+            index=self.simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_surface_store2",
         )
         self.store3 = pd.Series(
-            index=simulation.rainfall.index, name=f"{self.name}_surface_store3"
+            index=self.simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_surface_store3",
         )
-        self.runoff = pd.Series(
-            index=simulation.rainfall.index, name=f"{self.name}_surface_runoff"
-        )
-        self.baseflow = pd.Series(
-            index=simulation.rainfall.index, name=f"{self.name}_surface_baseflow"
+        self.imp_store = pd.Series(
+            index=self.simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_surface_imp_store",
         )
         self.et = pd.Series(
-            index=simulation.rainfall.index, name=f"{self.name}_surface_et"
+            index=self.simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_surface_et",
         )
-        self.groundwater = pd.Series(
-            index=simulation.rainfall.index, name=f"{self.name}_surface_groundwater"
+        self.excess = pd.Series(
+            index=self.simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_surface_excess",
+        )
+        self.runoff_store = pd.Series(
+            index=self.simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_surface_runoff_store",
+        )
+        self.runoff = pd.Series(
+            index=self.simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_surface_runoff",
+        )
+        self.baseflow_store = pd.Series(
+            index=self.simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_surface_baseflow_store",
+        )
+        self.baseflow = pd.Series(
+            index=self.simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_surface_baseflow",
+        )
+        self.total_runoff = pd.Series(
+            index=self.simulation.rainfall.index,
+            data=0.0,
+            name=f"{self.name}_surface_total_runoff",
         )
 
         # register with simulation
         self.simulation.surfaces[self.name] = self
 
-    def step(self, time):
+    def step(self):
+        time = self.simulation.time
+        time_previous = self.simulation.time_previous
+        rainfall = self.simulation.rainfall.loc[time]
+        pet = self.simulation.pet.loc[time]
 
-        self.runoff[time] = None  # update all series with value for current timestep
+        # add rainfall
+        self.store1.loc[time] = self.store1.loc[time_previous] + rainfall
+        self.store2.loc[time] = self.store2.loc[time_previous] + rainfall
+        self.store3.loc[time] = self.store3.loc[time_previous] + rainfall
+        self.imp_store.loc[time] = self.imp_store.loc[time_previous] + rainfall
+
+        # remove et & record
+        et1 = min(self.store1.loc[time], pet)
+        et2 = min(self.store2.loc[time], pet)
+        et3 = min(self.store3.loc[time], pet)
+        eti = min(self.imp_store.loc[time], pet)
+
+        self.store1.loc[time] = self.store1.loc[time] - et1
+        self.store2.loc[time] = self.store2.loc[time] - et2
+        self.store3.loc[time] = self.store3.loc[time] - et3
+        self.imp_store.loc[time] = self.imp_store.loc[time] - eti
+
+        et = (self.a1 * et1 + self.a2 * et2 + self.a3 * et3) * (1.0 - self.imp_frac) + (
+            eti * self.imp_frac
+        )
+        self.et.loc[time] = et
+
+        # calculate rainfall excess
+        if self.store1.loc[time] > self.c1:
+            e1 = self.store1.loc[time] - self.c1
+            self.store1.loc[time] = self.store1.loc[time] - e1
+        else:
+            e1 = 0.0
+        if self.store2.loc[time] > self.c2:
+            e2 = self.store2.loc[time] - self.c2
+            self.store2.loc[time] = self.store2.loc[time] - e2
+        else:
+            e2 = 0.0
+        if self.store3.loc[time] > self.c3:
+            e3 = self.store3.loc[time] - self.c3
+            self.store3.loc[time] = self.store3.loc[time] - e3
+        else:
+            e3 = 0.0
+        if self.imp_store.loc[time] > self.i:
+            ei = self.imp_store.loc[time] - self.i
+            self.imp_store.loc[time] = self.imp_store.loc[time] - ei
+        else:
+            ei = 0.0
+
+        excess = (self.a1 * e1 + self.a2 * e2 + self.a3 * e3) * (
+            1.0 - self.imp_frac
+        ) + (ei * self.imp_frac) * self.area
+        self.excess.loc[time] = excess
+
+        # route baseflow and runoff through stores
+        self.baseflow_store.loc[time] = (
+            self.baseflow_store.loc[time_previous] + excess * self.bfi
+        )
+        baseflow = (1.0 - self.kbase) * self.baseflow_store.loc[time]
+        self.baseflow.loc[time] = baseflow
+
+        self.runoff_store.loc[time] = self.runoff_store.loc[time_previous] + excess * (
+            1.0 - self.bfi
+        )
+        runoff = (1.0 - self.ksurf) * self.runoff_store.loc[time]
+        self.runoff.loc[time] = runoff
+
+        # calculate total flow and send to outlet
+        total_runoff = self.baseflow.loc[time] + self.runoff.loc[time]
+        self.total_runoff.loc[time] = total_runoff
+        self.outlet.put(total_runoff)
 
 
-class Mains:
+class Mains(Base):
     def __init__(self, simulation: Simulation):
 
         # initialise series to contain results
@@ -161,51 +330,62 @@ class Mains:
         return volume
 
 
-class Demand:
+class Demand(Base):
     def __init__(
         self,
         name: str,
         simulation: Simulation,
-        potable_demand: Union[pd.Series, None],
-        nonpotable_demand: Union[pd.Series, None],
+        demand: pd.Series,
         source: Storage,
         backup: Union[Mains, None],
-        wastewater_converstion_factor: float = 0.0,
+        wastewater_sink: Union[Storage, None],
+        wastewater_conversion_factor: float = 0.0,
     ):
         self.name = name
         self.simulation = simulation
-        self.potable_demand = potable_demand
-        self.nonpotable_demand = nonpotable_demand
+        self.demand = demand
         self.source = source
         self.backup = backup
-        self.wastewater_converstion_factor = Factor(wastewater_converstion_factor)
+        self.wastewater_sink = wastewater_sink
+        self.wastewater_conversion_factor = Factor(wastewater_conversion_factor)
 
         # initialise series to contain results
-        self.satisfied_demand = pd.Series(
-            index=simulation.rainfall.index,
-            data=0.0,
-            name=f"{self.name}_demand_satisfied",
+        self.source_supplied = pd.Series(
+            index=simulation.rainfall.index, data=0.0, name=f"{self.name}_demand_source"
         )
-        self.unsatisfied_demand = pd.Series(
+        self.backup_supplied = pd.Series(
+            index=simulation.rainfall.index, data=0.0, name=f"{self.name}_demand_backup"
+        )
+        self.wastewater_stream = pd.Series(
             index=simulation.rainfall.index,
             data=0.0,
-            name=f"{self.name}_demand_unsatisfied",
+            name=f"{self.name}_demand_wastewater_stream",
         )
 
         # register with simulation
         self.simulation.demands[self.name] = self
 
-    def step(self, time):
+    def step(self):
+        time = self.simulation.time
+        # satisfy demand first from the source then from backup if needed
         current_demand = self.demand[time]
-        satisfied_demand = self.source.take(current_demand)
-        unsatisfied_demand = current_demand - satisfied_demand
-        mains_demand = self.backup.take(unsatisfied_demand)
+        source_supplied = self.source.take(current_demand)
+        if self.backup:
+            backup_required = current_demand - source_supplied
+            backup_supplied = self.backup.take(backup_required)
+        else:
+            backup_supplied = 0.0
+        # divert specified portion of demand to wastewater
+        wastewater_stream = current_demand * self.wastewater_conversion_factor
+        if self.wastewater_sink:
+            self.wastewater_sink.put(wastewater_stream)
+        # record in results
+        self.source_supplied[time] = source_supplied
+        self.backup_supplied[time] = backup_supplied
+        self.wastewater_stream[time] = wastewater_stream
 
-        self.satisfied_demand[time] = satisfied_demand
-        self.unsatisfied_demand[time] = unsatisfied_demand
 
-
-class Pump:
+class Pump(Base):
     def __init__(
         self,
         name: str,
@@ -230,25 +410,16 @@ class Pump:
 
     def step(self):
         time = self.simulation.time
+
         # get scheduled flow for current timestep
         scheduled_flow = self.scheduled_flows[time]
+
         # take available flow from source and record it
         available_flow = self.source.take(scheduled_flow)
-        self.pumped_flows[time] = available_flow
+
         # transfer available flow to destination
         self.destination.put(available_flow)
 
-
-if __name__ == "__main__":
-    import numpy as np
-
-    timestep = pd.Timedelta("1 hour")
-    datetime_index = pd.date_range("2020-01-01", "2020-12-31", freq=timestep)
-    rainfall = pd.Series(
-        index=datetime_index, data=np.random.random(size=len(datetime_index))
-    )
-    pet = pd.Series(
-        index=datetime_index, data=np.random.random(size=len(datetime_index))
-    )
-    sim = Simulation(timestep=timestep, rainfall=rainfall, pet=pet)
+        # record in results
+        self.pumped_flows[time] = available_flow
 
--- wb_simulation.py
+++ wb_simulation.py
@@ -1,5 +1,9 @@
+# %%
+
 from typing import Union
 import pandas as pd
+from wb_nodes import *
+from tqdm import tqdm
 
 
 class Simulation:
@@ -19,14 +23,57 @@ class Simulation:
     def run(self):
         """main loop
         """
-        for time in self.rainfall.index:
+        # skip first timestep
+        for time in tqdm(self.rainfall.index[1:]):
             self.time = time
+            self.time_previous = self.rainfall.index[
+                self.rainfall.index.get_loc(time) - 1
+            ]
             # do all the things
-            for surface in self.surfaces:
-                surface.step()
-            for demand in self.demands:
-                demand.step()
-            for pump in self.pumps:
-                pump.step()
-            for storage in self.storages:
+            for storage in self.storages.values():
                 storage.step()
+            for surface in self.surfaces.values():
+                surface.step()
+            for demand in self.demands.values():
+                demand.step()
+            for pump in self.pumps.values():
+                pump.step()
+
+
+if __name__ == "__main__":
+    import numpy as np
+
+    timestep = pd.Timedelta("1 day")
+    datetime_index = pd.date_range("2010-07-24", "2020-12-31", freq=timestep)
+    rainfall = pd.Series(
+        index=datetime_index, data=np.random.random(size=len(datetime_index))
+    )
+    pet = pd.Series(
+        index=datetime_index, data=np.random.random(size=len(datetime_index))
+    )
+    demand = (
+        pd.Series(index=datetime_index, data=np.random.random(size=len(datetime_index)))
+        * 10000
+    )
+    sim = Simulation(timestep=timestep, rainfall=rainfall, pet=pet)
+
+    # surfaces
+    # sink = Storage("sink", sim, 0, 100000, 0, 1.3, None)
+    pond = Storage("pond", sim, 0, 100000, 500, 200, None)
+    # field1 = Demand("field1", sim, demand, pond, None, None, 0)
+    catchmentA = SurfaceAWBM("catchmentA", sim, 150001.0, 0.2, pond)
+
+    print(sim.surfaces)
+    print(sim.storages)
+    print(sim.demands)
+
+    sim.run()
+
+    # %%
+    pond.volume.plot()
+    # %%
+    pond.leakage.plot()
+    # %%
+    catchmentA.total_runoff.plot()
+
+# %%
